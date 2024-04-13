59b5ec95e134ab4ead8951f95df3cfbee5e83fd4
nikawakadze <wakadzenika@gmail.com>
2024-04-13 21:03:57 +0400

    commit

---
+++ day11/homework/homevork11.py
@@ -0,0 +1,34 @@
+list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in list:
+    print(list[i] * 2)
+
+for i in list:
+    print(list[i] / 2)
+
+for i in list:
+    print(list[i] + 2)
+
+for i in list:
+    print(list[i] - 2)
+
+
+list2 = ["home", "car" ,"bike", "dog"]
+
+print(list2[0])
+
+print(list2[1])
+
+print(list2[2])
+
+print(list2[3])
+
+
+
+products = ["coffe", 'coca-cola', "water", 'sprite']
+choice = int(input('Enter a num: '))
+print(products[choice])
+
+
+
+
+
--- day13/homework/homevorki13.py
+++ day13/homework/homevorki13.py
@@ -3,4 +3,5 @@ a = [1, 2, 3, 4, 5]
 
 for x in range(len(a)):
     print (a[x])
-    
+
+
